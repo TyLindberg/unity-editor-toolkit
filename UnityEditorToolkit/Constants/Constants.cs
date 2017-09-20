74dc5b4dce223f177b9f338b5eac297ae4b7d944
Tyler Lindberg <tlindberg@ucla.edu>
2017-09-20 15:42:24 -0700

    Added scene rotation commands

--- UnityEditorToolkit/Constants/Constants.cs
+++ UnityEditorToolkit/Constants/Constants.cs
@@ -7,20 +7,34 @@
 		private const string BaseMenuPath = "Toolkit/";
 		private const string SnappingMenuPath = "";
 		private const string ShadingModeMenuPath = "Shading Mode/";
+		private const string SceneRotationMenuPath = "Scene Rotation/";
 
 
 		//////////////////////////////
 		//  Menu Item Key Bindings  //
 		//////////////////////////////
 
+		// _ = raw key press
+		// % = Control(Windows)/Command(Mac)
+		// # = Shift
+		// & = Alt
+
 		// Snapping
 		private const string SnapToZeroKey = "_.";
 		private const string SnapToGroundKey = "_g";
 
 		// Shading Mode
-		private const string ShadedModeKey = "_3";
-		private const string WireframeModeKey = "_4";
-		private const string ShadedWireframeModeKey = "_5";
+		private const string ShadedModeKey = "_7";
+		private const string WireframeModeKey = "_8";
+		private const string ShadedWireframeModeKey = "_9";
+
+		// Scene Rotation
+		private const string RightViewKey = "_3";
+		private const string LeftViewKey = "#3";
+		private const string TopViewKey = "_4";
+		private const string BottomViewKey = "#4";
+		private const string FrontViewKey = "_5";
+		private const string BackViewKey = "#5";
 
 
 		//////////////////////////////
@@ -41,6 +55,20 @@
 		public const string ShadedWireframeModeItem = BaseMenuPath + ShadingModeMenuPath +
 			"Shaded Wireframe " + ShadedWireframeModeKey;
 
+		// Scene Rotation
+		public const string RightViewItem = BaseMenuPath + SceneRotationMenuPath +
+			"Right View " + RightViewKey;
+		public const string LeftViewItem = BaseMenuPath + SceneRotationMenuPath +
+			"Left View " + LeftViewKey;
+		public const string TopViewItem = BaseMenuPath + SceneRotationMenuPath +
+			"Top View " + TopViewKey;
+		public const string BottomViewItem = BaseMenuPath + SceneRotationMenuPath +
+			"Bottom View " + BottomViewKey;
+		public const string FrontViewItem = BaseMenuPath + SceneRotationMenuPath +
+			"Front View " + FrontViewKey;
+		public const string BackViewItem = BaseMenuPath + SceneRotationMenuPath +
+			"Back View " + BackViewKey;
+
 
 		//////////////////////////////
 		//   Menu Item Priorities   //
@@ -54,5 +82,13 @@
 		public const int ShadedModePriority = 50;
 		public const int WireframeModePriority = ShadedModePriority + 1;
 		public const int ShadedWireframeModePriority = WireframeModePriority + 1;
+
+		// Scene Rotation
+		public const int RightViewPriority = 55;
+		public const int LeftViewPriority = RightViewPriority + 1;
+		public const int TopViewPriority = LeftViewPriority + 1;
+		public const int BottomViewPriority = TopViewPriority + 1;
+		public const int FrontViewPriority = BottomViewPriority + 1;
+		public const int BackViewPriority = FrontViewPriority + 1;
 	}
 }
---
+++ UnityEditorToolkit/Editor/SceneViewMenuItems.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace UnityEditorToolkit {
+	public class SceneViewMenuItems : Editor {
+
+		static readonly string ShadedModeMessage = "Set to Shaded";
+		static readonly string WireframeModeMessage = "Set to Wireframe";
+		static readonly string ShadedWireframeModeMessage = "Set to Shaded Wireframe";
+
+		static string ShadingModeToMessage(DrawCameraMode mode) {
+			switch(mode) {
+			case DrawCameraMode.Textured:
+				return ShadedModeMessage;
+			case DrawCameraMode.Wireframe:
+				return WireframeModeMessage;
+			case DrawCameraMode.TexturedWire:
+				return ShadedWireframeModeMessage;
+			default:
+				Debug.LogError(mode + " is an unsupported DrawCameraMode");
+				break;
+			}
+
+			return "";
+		}
+
+		static SceneView GetSceneView() {
+			return SceneView.lastActiveSceneView;
+		}
+
+		// Shading Mode
+
+		static void SetSceneRenderMode(DrawCameraMode mode) {
+			SceneView currentScene = GetSceneView();
+			if(currentScene.renderMode != mode) {
+				Undo.RecordObject(currentScene, ShadingModeToMessage(mode));
+				currentScene.renderMode = mode;
+				currentScene.Repaint();
+			}
+		}
+
+		[MenuItem(Constants.ShadedModeItem, false, Constants.ShadedModePriority)]
+		static void SetShadedMode() {
+			SetSceneRenderMode(DrawCameraMode.Textured);
+		}
+
+		[MenuItem(Constants.ShadedModeItem, true)]
+		static bool CheckShadedMode() {
+			return GetSceneView() != null;
+		}
+
+		[MenuItem(Constants.WireframeModeItem, false, Constants.WireframeModePriority)]
+		static void SetWireframeMode() {
+			SetSceneRenderMode(DrawCameraMode.Wireframe);
+		}
+
+		[MenuItem(Constants.WireframeModeItem, true)]
+		static bool CheckWireframeMode() {
+			return GetSceneView() != null;
+		}
+
+		[MenuItem(Constants.ShadedWireframeModeItem, false, Constants.ShadedWireframeModePriority)]
+		static void SetShadedWireframeMode() {
+			SetSceneRenderMode(DrawCameraMode.TexturedWire);
+		}
+
+		[MenuItem(Constants.ShadedWireframeModeItem, true)]
+		static bool CheckShadedWireframeMode() {
+			return GetSceneView() != null;
+		}
+
+		// Scene Controls
+
+		static bool CanRotateScene(SceneView view) {
+			return view != null && !view.isRotationLocked;
+		}
+
+		static void RotateSceneView(SceneView view, Quaternion rot) {
+			if(CanRotateScene(view)) {
+				view.LookAt(view.pivot, rot);
+			}
+		}
+
+		[MenuItem(Constants.RightViewItem, false, Constants.RightViewPriority)]
+		static void SetRightView() {
+			RotateSceneView(GetSceneView(), Quaternion.Euler(0, -90, 0));
+		}
+
+		[MenuItem(Constants.RightViewItem, true)]
+		static bool CheckRightViewItem() {
+			return CanRotateScene(GetSceneView());
+		}
+
+		[MenuItem(Constants.LeftViewItem, false, Constants.LeftViewPriority)]
+		static void SetLeftView() {
+			RotateSceneView(GetSceneView(), Quaternion.Euler(0, 90, 0));
+		}
+
+		[MenuItem(Constants.LeftViewItem, true)]
+		static bool CheckLeftViewItem() {
+			return CanRotateScene(GetSceneView());
+		}
+
+		[MenuItem(Constants.TopViewItem, false, Constants.TopViewPriority)]
+		static void SetTopView() {
+			RotateSceneView(GetSceneView(), Quaternion.Euler(90, 0, 0));
+		}
+
+		[MenuItem(Constants.TopViewItem, true)]
+		static bool CheckTopViewItem() {
+			return CanRotateScene(GetSceneView());
+		}
+
+		[MenuItem(Constants.BottomViewItem, false, Constants.BottomViewPriority)]
+		static void SetBottomView() {
+			RotateSceneView(GetSceneView(), Quaternion.Euler(-90, 0, 0));
+		}
+
+		[MenuItem(Constants.BottomViewItem, true)]
+		static bool CheckBottomViewItem() {
+			return CanRotateScene(GetSceneView());
+		}
+
+		[MenuItem(Constants.FrontViewItem, false, Constants.FrontViewPriority)]
+		static void SetFrontView() {
+			RotateSceneView(GetSceneView(), Quaternion.Euler(0, 180, 0));
+		}
+
+		[MenuItem(Constants.FrontViewItem, true)]
+		static bool CheckFrontViewItem() {
+			return CanRotateScene(GetSceneView());
+		}
+
+		[MenuItem(Constants.BackViewItem, false, Constants.BackViewPriority)]
+		static void SetBackView() {
+			RotateSceneView(GetSceneView(), Quaternion.Euler(0, 0, 0));
+		}
+
+		[MenuItem(Constants.BackViewItem, true)]
+		static bool CheckBackViewItem() {
+			return CanRotateScene(GetSceneView());
+		}
+	}
+}
